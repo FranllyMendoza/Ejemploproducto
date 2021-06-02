9cc9678dfb5171e7ea2616aaa8651293839a8f26
Franlly Mendoza Fana <franlly_mendoza@ucne.edu.do>
2021-06-01 23:10:56 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Ejemploproducto.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejemploproducto.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Productos.db");
+        }
+    }
+}
---
+++ Models/Productos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejemploproducto.Models
+{
+    public class Productos
+    {
+        [Key]
+        [Required(ErrorMessage = "Este campo no puede estar vacío!")]
+        [Range(0, 10000, ErrorMessage = "Este campo debe ser mayor a 0.")]
+        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "Este campo no puede estar vacío!")]
+        [MinLength(1, ErrorMessage = "Este campo debe tener como mínimo 10 caracteres.")]
+        public string Descripcion { get; set; }
+       
+        [Range(0, 100000, ErrorMessage = "Este campo debe ser mayor a 0.")]
+        public float Existencia { get; set; }
+        [Range(0, 100000, ErrorMessage = "Este campo debe ser mayor a 0.")]
+        public float Costo { get; set; }
+        [Range(0, 100000, ErrorMessage = "Este campo debe ser mayor a 0.")]
+        public float  valorInventario { get; set; }
+
+    }
+}
